57c8286d6fe780c182a5d8332b6943c988bb895a
Jesus Varela <jesusaugusto.008@gmail.com>
2024-04-13 16:39:29 -0400

    feat: adapt titles styles

--- src/containers/products/index.tsx
+++ src/containers/products/index.tsx
@@ -4,29 +4,25 @@ import { MenuIcon } from "@/assets";
 import styles from "./index.module.scss";
 
 export const Products: FC = () => (
-  <div className={styles.Products}>
+  <section className={styles.Products}>
     <NavBar
       leftItem={<Button variant="default" icon={<MenuIcon />} />}
       rightItem={
-        <Button
-          variant="default"
-          icon={
-            <Avatar
-              src="/img/default-user.jpg"
-              alt="user_avatar"
-              width={40}
-              height={40}
-            />
-          }
+        <Avatar
+          src="/img/default-user.jpg"
+          alt="user_avatar"
+          width={40}
+          height={40}
         />
       }
-      title="Test Title"
     />
-    <h2>Hi Mr. Michael,</h2>
-    <h1>Welcome Back!</h1>
+    <div className={styles.titleWrapper}>
+      <h2>Hi Mr. Michael,</h2>
+      <h1>Welcome Back!</h1>
+    </div>
     <SearchInput
       placeholder="Search burger, pizza, drink or ect..."
       className={styles.inputStyle}
     />
-  </div>
+  </section>
 );
