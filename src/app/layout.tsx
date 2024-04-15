64e7f3deb71db08d49121f2526f1af02277f070e
Jesus Varela <jesusaugusto.008@gmail.com>
2024-04-14 23:24:10 -0400

    feat: change icon and general font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import styles from "./layout.module.scss";
 import cx from "classnames";
 
-const inter = Inter({ subsets: ["latin"] });
+const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Beer e-commerce",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cx(inter.className, styles.Layout)}>{children}</body>
+      <body className={cx(dmSans.className, styles.Layout)}>{children}</body>
     </html>
   );
 }
